b303168417753686cbf860d77286962fbf8df33d
李東霖 <lee097019@gmail.com>
2019-01-04 01:10:28 +0800

    Use this page to be welcome page

--- WEB/admin-gui/src/Dashboard.js
+++ WEB/admin-gui/src/Dashboard.js
@@ -2,14 +2,8 @@ import React from 'react';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import { Title,ReferenceInput, SelectInput, SimpleForm, Toolbar } from 'react-admin';
-import { LineChart, Line, Legend, XAxis, YAxis, Tooltip, ResponsiveContainer } from 'recharts';
 import ChangeButton from './ChangeButton'
-
-var data = [
-    {name : 0, value : 1},
-    {name : 3, value : 2},
-    {name : 6, value : 1},
-]
+import Typography from '@material-ui/core/Typography';
 
 // using handleSubmit to deal with custom behavior
 const DashBoardToolbar = props => (
@@ -27,22 +21,18 @@ export default (props) => (
     <Card {...props}>
         <Title title="Welcome to the administration" />
         <CardContent>
-        
+        <Typography component="p">
+            Hi everyone!!<br/>
+            this is a final project for IoT/M2M cource<br/>
+            you can find this project on <a href="https://github.com/csielee/IoT-M2M-cloud">GITHUB</a>
+        </Typography>
+        {/*
         <SimpleForm resource="resources"  toolbar={<DashBoardToolbar />} submitOnEnter={false}>
-        <ReferenceInput label="test" source="xdd" reference="resources" target="ty">
+        <ReferenceInput label="test" source="id" reference="resources">
             <SelectInput optionText={<RNField />} optionValue="id" />
         </ReferenceInput>
         </SimpleForm>
-
-        <ResponsiveContainer minWidth={200} width="99%" height={320} debounce={1} >
-            <LineChart data={data}>
-                <XAxis dataKey="name"/>
-                <YAxis />
-                <Tooltip />
-                <Legend />
-                <Line type="monotone" dataKey="value" stroke="#8884d8" />
-            </LineChart>
-        </ResponsiveContainer>
+        */}
         </CardContent>
     </Card>
 );
